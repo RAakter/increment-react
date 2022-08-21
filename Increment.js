b3a4790d405c95c9053a7ec1da5bf6facd626308
Rezowana Akter <rezowana10@gmail.com>
2022-08-21 19:28:18 +0600

    increment functionality added

--- Increment.js
+++ Increment.js
@@ -3,9 +3,31 @@
 
 const domContainer = document.querySelector("#root");
 
-const myElement = React.createElement("div", null, [
-    React.createElement("p", null, "Hi"),
-    React.createElement("p", null, "BYE!"),
-]);
+// const myElement = React.createElement("div", null, [
+//     React.createElement("p", null, "Hi"),
+//     React.createElement("p", null, "BYE!"),
+// ]);
 
-ReactDOM.render(myElement, domContainer);
+const Increment = () => {
+    const [counter, setCounter] = React.useState(0);
+    console.log(counter);
+    console.log(setCounter);
+    return (
+        <div>
+            <h1 id="display">{ counter }</h1>
+            <div>
+                <button id="btn" onClick= { () => setCounter(counter+1)}>Increment +</button>
+            </div>
+        </div>
+    )
+};
+
+// ReactDOM.render(<Increment/>, domContainer);
+
+ReactDOM.render(
+    <div className="container">
+        <Increment/>
+        <Increment/>
+        <Increment/>
+    </div>,
+domContainer);
